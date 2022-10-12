ee373c79461d4357839d85ce6e0b0c9ce1ec2dbf
mjmm39 <zzaa9988@naver.com>
2022-10-12 11:40:01 +0900

    mcinema

---
+++ script.js
@@ -0,0 +1,15 @@
+
+$("document").ready(function(){
+
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+ 
+
+  $("article").eq(2)
+  
+
+});
